83804ce8861ab6514615c2724beb9eb1f72825ee
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2022-11-22 05:52:48 -0300

    expressions and operators class

---
+++ expressions_and_operators/expressions_and_operators.js
@@ -0,0 +1,20 @@
+/* Expressões e Operadores
+
+- Expressions
+- Operators
+    Binary
+    Unary
+    Ternary
+*/
+
+let number = 1
+
+// Binary
+console.log(number + 1)
+
+// Unary
+console.log(++number)
+console.log(typeof number)
+
+// Ternary
+console.log(true ? 'true' : 'false')
