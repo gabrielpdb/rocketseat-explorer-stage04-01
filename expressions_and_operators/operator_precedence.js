93f26f9ed692e1fcb19c7c6c42c60d71ce6b1b03
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2022-11-22 06:59:02 -0300

    operator precedence

---
+++ expressions_and_operators/operator_precedence.js
@@ -0,0 +1,13 @@
+/* Operator precedence
+
+  * grouping                      ( )
+  * negação e incremento          ! ++ ==
+  * multiplicação e divisão       * /
+  * adição e subtração            + -
+  * relacional                    < <= > >=
+  * igualdade                     == != === !==
+  * AND                           &&
+  * OR                            ||
+  * condicional                   ?:
+  * assignment (atribuição)       = += -= *=
+*/
--- expressions_and_operators/scripts.js
+++
@@ -1,7 +0,0 @@
-// Operador de String (String operator)
-
-// comparison (comparação)
-console.log('a' == 'b')
-
-// concatenation (concatenação)
-console.log('a' + 'a')
