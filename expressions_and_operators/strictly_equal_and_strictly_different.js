d4d6af838a3e7cdd44a190c21d593c52f0521628
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2022-11-22 06:18:28 -0300

    strictly equal and strictly different

--- expressions_and_operators/strictly_equal_and_strictly_different.js
+++ expressions_and_operators/strictly_equal_and_strictly_different.js
@@ -5,10 +5,10 @@
 let one = 1
 let two = 2
 
-// estritamente igual a ==
+// estritamente igual a ===
 console.log(one === '1')
 console.log(two === 1)
 
-// estritamente diferente de !=
+// estritamente diferente de !==
 console.log(two !== '2')
 console.log(two !== 2)
