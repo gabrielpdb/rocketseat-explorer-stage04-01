f2071bb3db5a2bebc0e35a99835d3d45cb807ece
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2022-11-22 05:38:08 -0300

    done

---
+++ functions/functions_1.js
@@ -0,0 +1,11 @@
+// Criar um aplicativo de frases motivacionais
+
+// Function Statement
+function createPhrases() {
+  console.log('Estudar é muito bom')
+  console.log('Paciência e persistência')
+  console.log('Revisão é a mãe do aprendizado')
+}
+
+// Execute, run, call, invoke
+createPhrases()
