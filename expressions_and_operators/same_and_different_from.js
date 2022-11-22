1899efd0bf62be20a11392e349f4ce3a6ffa470d
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2022-11-22 06:15:25 -0300

    same and different from

--- expressions_and_operators/same_and_different_from.js
+++ expressions_and_operators/same_and_different_from.js
@@ -10,6 +10,6 @@ console.log(two == 1)
 console.log(one == '1')
 
 // diferente de !=
-console.log(one != two);
-console.log(one != 1);
-console.log(one != "1");
+console.log(one != two)
+console.log(one != 1)
+console.log(one != '1')
